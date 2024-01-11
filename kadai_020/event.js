6d21adc3d11f45e6796ba7ed76db162be9fbffa8
Yamahiro-Frandol <148076928+Yamahiro-Frandol@users.noreply.github.com>
2024-01-11 23:01:53 +0900

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById('btn');
 const text = document.getElementById('text');
-btn.addEventListener('click', (Event) => {
+btn.addEventListener('click', () => {
     text.textContent = 'ボタンをクリックしました';
     console.log(text);
 })
